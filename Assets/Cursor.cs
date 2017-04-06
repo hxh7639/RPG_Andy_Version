ad2365cf506a84a8e8328c678d514ebe7d8ee5b7
Andy <hxh7639@yahoo.com.tw>
2017-04-05 22:32:33 -0500

    RPG - 010 end

---
+++ Assets/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(cameraRaycaster.layerHit);
+		
+	}
+}
