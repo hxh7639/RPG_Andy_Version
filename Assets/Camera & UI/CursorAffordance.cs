58b50dfe270dba2614c792f630a3959df979b622
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-04-08 18:48:24 -0500

    RPG 014-end

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField] Texture2D walkCursor = null;
+    [SerializeField] Texture2D TargetCursor = null;
+    [SerializeField] Texture2D UnknownCursor = null;
+
+	CameraRaycaster cameraRaycaster;
+    [SerializeField] Vector2 cursorHotspot = new Vector2 (96,96);
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log(cameraRaycaster.layerHit);
+        switch (cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(TargetCursor, cursorHotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(UnknownCursor, cursorHotspot, CursorMode.Auto);
+                break;
+
+            default:
+                Debug.LogError("dont know what cursor to show");
+
+                return;
+        }
+	}
+}
