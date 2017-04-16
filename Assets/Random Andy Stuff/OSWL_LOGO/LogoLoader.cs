22bb768111f7d7fcfa2ce23e7bf5dd4009cc2620
Andy <hxh7639@yahoo.com.tw>
2017-04-16 18:38:43 -0500

    RPG 038 - 1

---
+++ Assets/Random Andy Stuff/OSWL_LOGO/LogoLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("countDown");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private IEnumerator countDown(){
+		yield return new WaitForSeconds(9f);
+		Application.LoadLevel(1);
+	}
+}
