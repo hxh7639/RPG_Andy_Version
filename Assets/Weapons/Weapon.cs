4991efd37b035f250829033ddc5823995d20ec59
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-06-10 19:43:40 -0500

    RPG - 077 Scriptable Objects

---
+++ Assets/Weapons/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = ("RPG/Weapon"))]
+public class Weapon : ScriptableObject {
+
+    [SerializeField] GameObject WeaponPrefab;
+    [SerializeField] AnimationClip attackAnimation;
+
+
+}
