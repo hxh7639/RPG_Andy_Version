ad5ed24cd58699ac4a8b07fa31cc857b1dfa727c
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-07-30 19:56:20 -0500

    RPG 103 Extension by composition (Energy Bar coding) - End

--- Assets/_Characters/Player/Energy.cs
+++ Assets/_Characters/Player/Energy.cs
@@ -2,27 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using RPG.CameraUI;
 
 namespace RPG.Characters
 {
     public class Energy : MonoBehaviour
     {
-        [SerializeField] RawImage energyBar;
+        [SerializeField] RawImage energyBarImage;
         [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] int energyToSpend = 10;
 
+        CameraRaycaster cameraRaycaster;
         float currentEnergyPoints;
         
         // Use this for initialization
         void Start()
         {
             currentEnergyPoints = maxEnergyPoints;
-
+            cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
+            cameraRaycaster.notifyRightClickObservers += ProcessRightClick; // Register right mouse click
         }
 
-        // Update is called once per frame
-        void Update()
+        void ProcessRightClick(RaycastHit raycastHit, int layerHit)
         {
+            SpendEnergy(energyToSpend);
+            print(currentEnergyPoints);
+            UpdateEnergyBar();
+        }
 
+        public void SpendEnergy(float energySpentPerUse)
+        {
+            float newEnergyPoints = currentEnergyPoints - energySpentPerUse;
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0f, maxEnergyPoints);
+        }
+
+        private void UpdateEnergyBar()
+        {
+            float xValue = -(EnergyAsPercentage() / 2f) - 0.5f;
+            energyBarImage.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+        }
+
+        float EnergyAsPercentage()
+        {
+                return currentEnergyPoints / maxEnergyPoints;
         }
     }
 }
