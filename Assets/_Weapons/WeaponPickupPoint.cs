13dd8fb3dfb276d1f6f54a6d154f995b875a5aea
Andy <hxh7639@yahoo.com.tw>
2017-10-18 22:10:29 -0500

    RPG - 143. Weapon Pickup Points - 1

---
+++ Assets/_Weapons/WeaponPickupPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    public class WeaponPickupPoint : MonoBehaviour
+    {
+        [SerializeField] Weapon weaponConfig;
+        [SerializeField] AudioClip pickUpSFX;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
