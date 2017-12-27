67643472b27ae4e0090666ba83cfa225007f72fa
DESKTOP-O7C4PR2\Uno <hxh7639@yahoo.com.tw>
2017-12-27 15:50:52 -0600

    RPG 162. AI Behaviour Tree In Unity

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -5,27 +5,61 @@ using UnityEngine;
 
 namespace RPG.Characters
 {
+    [RequireComponent(typeof(Character))]
     [RequireComponent (typeof(WeaponSystem))]
     public class EnemyAI : MonoBehaviour
     {        
         [SerializeField] float chaseRadius = 15f;
 
-        bool isAttacking = false; // TODO more rich state
         PlayerMovement player = null;
+        Character character;
         float currentWeaponRange;
+        float distanceToPlayer;
+
+        enum State { idle, patrolling, attacking, chasing}
+        State state = State.idle;
         
         void Start()
         {
+            character = GetComponent<Character>();
             player = FindObjectOfType<PlayerMovement>();
         }
         
         void Update()
         {
             // to walk to player (set player as target)
-            float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+            distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
             currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
+            if (distanceToPlayer > chaseRadius && state != State.patrolling)
+            {
+                StopAllCoroutines();
+                state = State.patrolling;
+            }
+            if (distanceToPlayer <= chaseRadius && state!= State.chasing)
+            {
+                StopAllCoroutines();
+                StartCoroutine(ChasePlayer());
+            }
+            if (distanceToPlayer <= currentWeaponRange && state != State.attacking)
+            {
+                StopAllCoroutines();
+                state = State.attacking;
+            }
+
         }
+
+        IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            while (distanceToPlayer >= currentWeaponRange)
+            {
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
+            }
+
+        }
+
 
         //changed the way projectile works in RPG 161 simplifying enemy to enemyAI
 
